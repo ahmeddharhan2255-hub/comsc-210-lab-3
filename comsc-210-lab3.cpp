51d7672c071f791067194c7a3e0c4335d59ace2f
Ahmad Dharhan <ahmad@Ahmads-MacBook-Air-3.local>
2026-09-03 12:45:43 -0700

    Second commit

--- comsc-210-lab3.cpp
+++ comsc-210-lab3.cpp
@@ -1,1 +1,20 @@
-/
+// COMSC-210 | Lab 3 | Ahmad Dharhan
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Resturant
+{
+    string name;
+    string address;
+    int max_occupancy;
+    int meal_options;
+    string owner;
+
+
+};
+
+int main(){
+
+}
